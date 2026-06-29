1e3690e5845447c54fbbaae59f06f84483c1ca07
Guilherme <guilhermefemix2010@gmail,com>
2026-06-29 19:39:19 -0300

    3° exercício

---
+++ Login.py
@@ -0,0 +1,8 @@
+Usuario = input("Digite seu usuário: ")
+senha = input("Digite sua senha: ")
+
+if Usuario == "Guilherrme" and senha == "5521":
+    print("Login feito com sucesso! ")
+else: 
+    print("Usuário ou senha incorretos! ")
+    
