aaa0f58e3e9572a7b083909c2ec36dd9f0a05289
Guilherme <guilhermefemix2010@gmail,com>
2026-07-07 22:55:39 -0300

    primeiro contato com o "while"

---
+++ Fundamentos/08_senhawhile.py
@@ -0,0 +1,5 @@
+senha=input("Digite a senha: ")
+while senha != "244grau":
+    print("Senha incorreta. Tente novamente. ")
+    senha=input("Digite sua senha: ")
+print("senha correta. Bem-Vindo(a) ao sistema! ")
