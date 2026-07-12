47bb9015408fcfff9898c36f73174b779157861f
Guilherme <guilhermefemix2010@gmail,com>
2026-07-12 20:09:09 -0300

    Adicionei o while e automatizei o caixa eletrônico

--- Fundamentos/04_caixa_eletrônico.py
+++ Fundamentos/04_caixa_eletrônico.py
@@ -2,18 +2,21 @@ saldo = int (5000)
 print("Seja bem vindo ao caixa eletrônico! ")
 opção = int(input("Digite 1 para consultar o saldo, 2 para sacar, 3 para depositar e 4 para sair: "))
 
-if(opção == 1):
-    print("seu saldo é de: RS ", saldo)
-elif(opção == 2):
-    saque = int(input("Digite o valor que deseja sacar: "))
-    if(saque > saldo):
-        print ("Saldo insuficiente! ")
-    else:
-        saldo = saldo - saque
-        print("Saque realizado com sucesso!, seu saldo é de RS ", saldo)
-elif(opção == 3 ):
-    deposito = int(input("Digite o valor que deseja depositar: "))
-    saldo = saldo + deposito
-    print("Depósito realizado com sucesso!, seu saldo é de RS ", saldo)
-elif(opção == 4):
-    print("Obrigado por utilizar o caixa eletrônico! ")
+while opção != 4:
+    if(opção == 1):
+        print("seu saldo é de: RS ", saldo)
+    elif(opção == 2):
+        saque = int(input("Digite o valor que deseja sacar: "))
+        if(saque > saldo):
+            print ("Saldo insuficiente! ")
+        else:
+            saldo = saldo - saque
+            print("Saque realizado com sucesso!, seu saldo é de RS ", saldo)
+    elif(opção == 3 ):
+        deposito = int(input("Digite o valor que deseja depositar: "))
+        saldo = saldo + deposito
+        print("Depósito realizado com sucesso!, seu saldo é de RS ", saldo)
+    print("Seja bem vindo ao caixa eletrônico! ")
+    opção = int(input("Digite 1 para consultar o saldo, 2 para sacar, 3 para depositar e 4 para sair: "))
+   
+print("Obrigado por utilizar o caixa eletrônico! ")
